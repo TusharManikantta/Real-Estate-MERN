54b6a098eabe48bac7a7779c88c0ba930dbbc414
TusharManikantta <tusharmanikantta@gmail.com>
2025-05-20 01:29:41 +0530

    DAY-1

---
+++ src/routes/homePage/homePage.jsx
@@ -0,0 +1,26 @@
+import "./homePage.scss";
+
+function HomePage() {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get your Dream Place</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla
+            explicabo assumenda possimus tempore neque. Eum quibusdam, rerum qui
+            id eos quisquam eaque veniam sit vitae amet maxime et iure pariatur,
+            accusantium assumenda veritatis eveniet! Saepe, fuga. Nihil neque,
+            adipisci, et porro cumque odio dolore, in laborum beatae eius autem
+            non!
+          </p>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="Home Page" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
