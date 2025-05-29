7baaa6e133cc85236500a1bf842514c3563a26d0
TusharManikantta <tusharmanikantta@gmail.com>
2025-05-29 20:50:46 +0530

    UI Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
-import "./layout.scss";
-import Navbar from "./components/navbar/Navbar";
 import HomePage from "./routes/homePage/homePage";
+import Layout from "./routes/layout/layout";
+import ListPage from "./routes/listPage/listPage";
+import ProfilePage from "./routes/profilePage/profilePage";
+import SinglePage from "./routes/singlePage/SinglePage";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="content">
-        <HomePage />
-      </div>
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/list",
+          element: <ListPage />,
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />,
+        },
+        {
+          path: "/profile",
+          element: <ProfilePage />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
